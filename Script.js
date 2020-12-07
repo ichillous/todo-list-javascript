ed365f80a53f4cce01aace179e3400905ed1e564
ichillous <isiah.chillous@gmail.com>
2020-12-06 18:48:28 -0600

    added create functionality

--- Script.js
+++ Script.js
@@ -1,3 +1,8 @@
-const createList = () => {
-    document.getElementById("list")
+//Create a clone of of div
+function createList() {
+    const list = document.querySelector("#list")
+    document.addEventListener("click", createList)
+    document.body.appendChild(list.cloneNode(true))
 }
+
+
